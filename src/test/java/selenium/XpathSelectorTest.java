3e73d87da74bd89ddd825a16306a2eb998e929c8
tomaszgorski <tomasz.gorski88@gmail.com>
2024-11-18 21:04:03 +0100

    Xpath selector test class

---
+++ src/test/java/selenium/XpathSelectorTest.java
@@ -0,0 +1,16 @@
+package selenium;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class XpathSelectorTest {
+
+
+
+}
