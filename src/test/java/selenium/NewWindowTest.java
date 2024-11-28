97accd7b44a5a3726c7cfb18d327d806615695d3
tomaszgorski <tomasz.gorski88@gmail.com>
2024-11-28 16:28:00 +0100

    get Attribute value alerts handle iframe handle

---
+++ src/test/java/selenium/NewWindowTest.java
@@ -0,0 +1,42 @@
+package selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class NewWindowTest {
+
+
+    public static void printer(WebElement webElement){
+        System.out.println(webElement.hashCode());
+    }
+
+    public static void listPrinter(List<WebElement> webElementsList) {
+        for (int i = 0; i < webElementsList.size(); i++) {
+            System.out.print(i+1+". ");
+            printer(webElementsList.get(i));
+        }
+    }
+    @Test
+    public void testNewWindow() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver.get("https://testeroprogramowania.github.io/selenium/");
+
+        String startPage = driver.getWindowHandle();
+
+        driver.findElement(
+                By.linkText("Strona z iframe"))
+                .click();
+        WebElement iframe = driver.findElement(By.cssSelector("[src='basics.html']"));
+        driver.switchTo().frame(iframe);
+
+        driver.findElement(By.id("fname")).sendKeys("Tomek");
+        driver.switchTo().defaultContent();
+        System.out.println(driver.findElement(By.tagName("h1")).getText());
+
+    }
+}
